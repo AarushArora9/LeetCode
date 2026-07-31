a053791f25363d7481785bddf837865bfb459f94
Aarush Arora <aarusharoralko1@gmail.com>
2026-08-01 00:52:28 +0530

    Create P3016. Minimum Number of Pushes to Type Word II.java

---
+++ P3016. Minimum Number of Pushes to Type Word II.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int freq[]=new int[26];
+        for(int i=0;i<word.length();i++)
+        freq[word.charAt(i)-'a']++;
+        Arrays.sort(freq);
+        int ans=0;
+        for(int i=0;i<26;i++)
+        ans+=freq[i]*((25-i)/8+1);
+        return ans;
+    }
+}
